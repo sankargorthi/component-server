003d3ca7a7315d230f4ba89312c14113bc1a24d5
Milan Iliev <milaniliev@gmail.com>
2013-11-15 00:33:30 -0500

    Added config.json
    Also, used path.join for concatenating file paths (cross-platform FTW!)

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var sys = require('sys');
 var exec = require('execSync').exec;
+var fs = require('fs');
+var Path = require('path')
+
+var config = JSON.parse(fs.readFileSync(Path.join(__dirname, 'config.json')))
 
 app.get('/components/:username/:component/:version/*', function(req, res, next) {
     var params = req.params;
@@ -20,14 +24,14 @@ app.get('/', function(req, res, next) {
     res.send('Component server');
 });
 
-app.listen('3917');
+app.listen(config.port);
 
 /* Utils */
 function getRawFile(callback, username, component, version, file) {
-     var path = ['/data1/Users/git/repositories/',
+     var path = Path.join(config.repositories_path,
                    username,
                    component + '.git'
-                ].join('/');
+                );
      try {
          var result = exec('git --git-dir ' + path + ' show ' + version + ':' + file);
          if (!result.stderr && result.stdout.indexOf('Not a git repository') === -1) {
