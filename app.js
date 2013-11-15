48433eb19dfd54cb6d892206f3d41ccaf26ab642
Sankar Gorthi <sbgorthi@gmail.com>
2013-11-14 22:56:38 -0800

    Lints the file and simplifies the code
    - `config.json` can be read with a simple require
    - Also adds jslint directives

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
-var express = require('express');
-var app = express();
-var sys = require('sys');
-var exec = require('execSync').exec;
-var fs = require('fs');
-var Path = require('path')
+/*properties
+    component, exec, get, indexOf, join, length, listen, log, params, port,
+    repositories_path, send, stderr, stdout, username, version
+*/
+/*jslint node: true, passfail: false, nomen: true */
 
-var config = JSON.parse(fs.readFileSync(Path.join(__dirname, 'config.json')))
+'use strict';
+var express = require('express'),
+    app = express(),
+    sys = require('sys'),
+    exec = require('execSync').exec,
+    fs = require('fs'),
+    path = require('path'),
+    config = require('./config.json');
 
-app.get('/components/:username/:component/:version/*', function(req, res, next) {
+/* Utils */
+function getRawFile(callback, username, component, version, file) {
+    var result,
+        repo = path.join(config.repositories_path,
+            username,
+            component + '.git');
+    try {
+        result = exec('git --git-dir ' + repo + ' show ' + version + ':' + file);
+        if (!result.stderr && result.stdout.indexOf('Not a git repository') === -1) {
+            callback(result.stdout);
+        } else {
+            callback();
+        }
+    } catch (e) {
+        console.log(e);
+        callback();
+    }
+}
+
+app.get('/components/:username/:component/:version/*', function callback(req, res) {
     var params = req.params;
 
     if (params.length > 0) {
-        var file = getRawFile(function(data) {
+        getRawFile(function responder(data) {
             if (!data) {
                 res.send('File not found', 404);
             }
             res.send(data);
-        }, params.username, params.component, params.version, params[0]); 
+        }, params.username, params.component, params.version, params[0]);
     }
 });
 
-app.get('/', function(req, res, next) {
-    res.send('Component server');
+app.get('/', function defResponse(req, res) {
+    res.send('Component server', req);
 });
 
 app.listen(config.port);
-
-/* Utils */
-function getRawFile(callback, username, component, version, file) {
-     var path = Path.join(config.repositories_path,
-                   username,
-                   component + '.git'
-                );
-     try {
-         var result = exec('git --git-dir ' + path + ' show ' + version + ':' + file);
-         if (!result.stderr && result.stdout.indexOf('Not a git repository') === -1) {
-             callback(result.stdout);
-         } else {
-             callback();
-         }
-     } catch (e) {
-         console.log(e);
-         callback();
-     }
-}
 
